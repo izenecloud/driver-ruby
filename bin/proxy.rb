2bfcda6280f70a5ab7c1cd0b402f97243c3d447f
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2010-07-12 16:05:11 +0800

    avoid of connect to multiple cobra processes for proxy

--- bin/proxy.rb
+++ bin/proxy.rb
@@ -23,25 +23,19 @@ CONFIG[:Cobras].each do |cobra|
     conn = Sf1Driver::Connection.new(ba[:IP], ba[:Port])
     response = conn.send('schema',{})
     response["collections"].each do|collection|
-      COLLECTIONMAP[collection] = cobra[0]
+      COLLECTIONMAP[collection] = [cobra[0],ba[:IP],ba[:Port]]
     end
   end 
 end
 
 Proxy.start(:host => "0.0.0.0", :port => CONFIG[:Port], :debug => false) do |conn|
   @start = Time.now
-  @data = Hash.new("")
   
-  CONFIG[:Cobras].each do |cobra|
-    ba = cobra[1]
-    if ba.is_a? Hash
-      conn.server cobra[0], :host => ba[:IP], :port => ba[:Port]
-    end 
-  end
-
   conn.on_data do |data|
     request = JSON.parse(data.slice(Sf1Driver::Connection::INT_SIZE * 2,data.size))
-    [data,COLLECTIONMAP[request["collection"]]]
+    serverinfo = COLLECTIONMAP[request["collection"]]
+    conn.server serverinfo[0], :host => serverinfo[1], :port => serverinfo[2]
+    [data,serverinfo[0]]
   end
  
   conn.on_response do |server, resp|
@@ -50,7 +44,6 @@ Proxy.start(:host => "0.0.0.0", :port => CONFIG[:Port], :debug => false) do |con
 
   conn.on_finish do |name|
     p [:on_finish, name, Time.now - @start]
-    p @data
   end
 end
 
