61b239aa31e7ce03a62d91708a3dc9839a65d11e
Jia Guo <guojia@gmail.com>
2013-01-18 11:29:52 +0800

    tiny

--- lib/b5m-util/b5m_mail.rb
+++ lib/b5m-util/b5m_mail.rb
@@ -2,13 +2,18 @@ require 'mail'
 
 class B5mMail
   def self.send(opts)
+    from_str = opts[:from]
+    unless opts[:from_alias].nil?
+      from_str = "#{opts[:from_alias]} <#{from_str}>"
+    end
     mail = Mail.new do
-      from opts[:from]
+      from from_str
       to opts[:to]
       subject opts[:subject]
       body opts[:body]
     end
     Net::SMTP.start(opts[:host]) do |smtp|
       smtp.send_message(mail.to_s, opts[:from], opts[:to])
+    end
   end
 end
